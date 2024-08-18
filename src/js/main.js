979047a9ddf61a7282414952295de50763387b2c
Suraj Maity <surajmaity981@gmail.com>
2024-08-18 11:52:50 +0530

    lift-state, open-close door & lift scheduler added

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,6 @@ const floors = [];
 const lifts = [];
 const scheduleOperation = [];
 
-const eachLiftState = {
-    liftId: 0,
-    stateStatus: false,
-    movingStatus: false,
-    whereMoving: null,
-    floorNumberAt: 0,
-    element: null
-};
-
 playButton.addEventListener('click', function(event) {
     if (noOfFloors.value < 2 || noOfLifts.value < 1) {
         return;
@@ -99,30 +90,55 @@ function createLifts(totalNoOfLifts) {
         eachLift.appendChild(liftLeftDoor);
         eachLift.appendChild(liftRightDoor);
 
-        eachLiftState.liftId = `lift_${mainLiftNumber}`;
-        eachLiftState.stateStatus = false;
-        eachLiftState.element = eachLift;
+        const eachLiftState = {
+            liftId: mainLiftNumber,
+            stateStatus: false,
+            movingStatus: false,
+            whereMoving: null,
+            floorNumberAt: 0,
+            element: eachLift
+        };
 
         lifts.push(eachLiftState);
         groundFloor.appendChild(eachLift);
     }
 }
 
+function openDoorsOfCurrentLift(currentLiftId) {
+    const liftLeftDoor = document.querySelector(`#left_lift_door_${currentLiftId}`);
+    const liftRightDoor = document.querySelector(`#right_lift_door_${currentLiftId}`);
+
+    // open door
+    setTimeout(() => {
+        liftLeftDoor.style.transform = 'translateX(-100%)';
+        liftLeftDoor.style.transition = `transform 2.5s`;
+        liftRightDoor.style.transform = 'translateX(100%)';
+        liftRightDoor.style.transition = `transform 2.5s`;
+    }, 2500);
+
+    // close door
+    setTimeout(() => {
+        liftLeftDoor.style.transform = 'translateX(0)';
+        liftLeftDoor.style.transition = `transform 2.5s`;
+        liftRightDoor.style.transform = 'translateX(0)';
+        liftRightDoor.style.transition = `transform 2.5s`;
+    }, 5000);
+}
+
 function allLiftsButtonsControler(event) {
     const button = event.target.id;
-    const floorsLength = floors.length;
-    const floorNumberAt = floorsLength - button.split('#')[1] + 1;
-    console.log(floorNumberAt);
+    const floorNumberAt = button.split('_')[2] - 1;
 
     // check any lift present at same floor from where button is clicked
     const currentLiftAt = lifts.find(
         (lift) => {
-            lift.floorNumberAt === floorNumberAt && lift.movingStatus === false
+            return lift.floorNumberAt === floorNumberAt && lift.movingStatus === false
         }
     );
 
     if (currentLiftAt) {
-        openDoorsOfCurrentLift(currentLiftAt.id);
+        openDoorsOfCurrentLift(currentLiftAt.liftId);
+        return;
     }
 
     // check any lift moving at that floor from where button is clicked
@@ -139,6 +155,52 @@ function allLiftsButtonsControler(event) {
     scheduleOperation.push(floorNumberAt);
 }
 
-function openDoorsOfCurrentLift(currentLiftId) {
-    
+function getNearestLiftId (currentFloor) {
+
+    // finding nearest lift id
+    let distance = floors.length;
+    let id = lifts[0].liftId;
+    const totalNoOfLifts = lifts.length;
+
+    for (let liftIdx = 0; liftIdx < totalNoOfLifts; liftIdx++) {
+        const eachLift = lifts[liftIdx];
+
+        if (eachLift.stateStatus === false && Math.abs(eachLift.floorNumberAt - currentFloor) < distance) {
+            id = eachLift.liftId;
+            distance = Math.abs(eachLift.floorNumberAt - currentFloor);
+        }
+    }
+
+    return id;
+}
+
+function liftOperationScheduler() {
+    if (scheduleOperation.length === 0) {
+        return;
+    }
+
+    const getLift = scheduleOperation.shift();
+    const id = getNearestLiftId(getLift);
+
+    const lift = lifts.find(
+        (lift) => lift.liftId === id
+    );
+
+    if (!lift) {
+        scheduleOperation.unshift(getLift);
+        return;
+    }
+
+    liftMovement(lift.floorNumberAt, getLift, id);
+}
+
+function liftMovement(source, destination, currentLiftId) {
+    const currentLift = lifts.find(
+        (lift) => lift.liftId === id
+    );
+
+    const liftLeftDoor = document.querySelector(`#left_lift_door_${currentLiftId}`);
+    const liftRightDoor = document.querySelector(`#right_lift_door_${currentLiftId}`);
+
+
 }
